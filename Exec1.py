5ebb579ba3ab438ad84827f993b2e63a6fbd291c
Guilherme F <88292977+Guiisantoos@users.noreply.github.com>
2025-02-24 14:22:49 +0000

    Exercício 1 Python

---
+++ Exec1.py
@@ -0,0 +1,9 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print(f"Soma: {num1 + num2}")
+print(f"Multiplicação:{num1 * num2}")
+print(f"Divisão:{num1 / num2}")
+print(f"Divisão inteira:{num1 // num2}")
+print(f"Módulo(resto da divisão):{num1 % num2}")
+print(f"Exponenciação:{num1 ** num2}")
